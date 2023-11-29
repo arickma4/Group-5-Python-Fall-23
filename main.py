82695c4e746300570d95ae6d6cd1ce008f4208c8
arickma4 <arickma4@students.kennesaw.edu>
2023-11-29 16:05:10 -0500

    ahhhhh

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ import random
 import math
 import sys
 import pygame._sdl2.controller
+import pygame.gfxdraw
 
 pygame.init()
 
 # creating screen
-screen = pygame.display.set_mode((800, 800))
-
+screen = pygame.display.set_mode((0, 0))
+#Font for endgame
+fontL = pygame.font.Font('OCR.ttf', 50)
+fontS = pygame.font.Font('OCR.ttf', 30)
+WinT = fontL.render('Congrats! You Won!', True, (240,0,0))
+LoseT = fontL.render('Game Over', True, (240,0,0))
+#LoseT = font.render('Game Over', True, green, blue)
+#WinT = font.render('Congrats!', True, green, blue)
 # Title and icon
 pygame.display.set_caption('Jets!')
 icon = pygame.image.load('rocket(1).png')
 pygame.display.set_icon(icon)
 running = True
 # background
-background_img = pygame.image.load('space_bg.jpg')
+background_img = pygame.image.load('Background.png')
 # Player
 player_icon = pygame.image.load('Jet.png')
-playerX = 360
+playerX = (screen.get_width()/2)
 playerY = 630
 playerX_co = 0
 playerY_co = 0
@@ -29,9 +36,10 @@ enemyY = []
 enemyX_co = []
 enemyY_co = []
 spawn = 20
+Score = 100000
 for i in range(spawn):
     enemy_icon.append(pygame.image.load('Alien Ship.png'))
-    enemyX.append(random.randint(0, 735))
+    enemyX.append(random.randint(((screen.get_width()/4)), ((screen.get_width()/4)*3)))
     enemyY.append(random.randint(50, 150))
     enemyX_co.append(1)
     enemyY_co.append(40)
@@ -52,6 +60,7 @@ for x in range(pygame.joystick.get_count()):
 #Audio Init
 Bang = pygame.mixer.Sound('Bang.mp3')
 Boom = pygame.mixer.Sound('Boom.mp3')
+
 
 def player(x, y):
     screen.blit(player_icon, (x, y))
@@ -77,9 +86,9 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
 
 # game loop
 while running:
-
+    Score -= 10
     screen.fill((0, 0, 0))
-    screen.blit(background_img, (0, 0))
+    screen.blit(background_img, ((screen.get_width())/4, 0))
     for task in pygame.event.get():
         if task.type == pygame.QUIT:
             running = False
@@ -145,10 +154,10 @@ while running:
 
     # player
     playerX += playerX_co
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
-        playerX = 736
+    if playerX <= (screen.get_width()/4):
+        playerX = (screen.get_width()/4)
+    elif playerX >= ((screen.get_width()/4)*3):
+        playerX = ((screen.get_width()/4)*3)
     if playerY <= 0:
         playerY = 0
     elif playerY >= 736:
@@ -157,7 +166,7 @@ while running:
     # enemy co-ordinates
     for i in range(spawn):
         enemyX[i] += enemyX_co[i]
-        if enemyX[i] <= 0 or enemyX[i] >= 736:
+        if enemyX[i] <= (screen.get_width()/4) or enemyX[i] >= ((screen.get_width()/4)*3):
             if abs(enemyX_co[i]) < 5:
               enemyX_co[i] = enemyX_co[i] * -1.1
             else:
@@ -165,16 +174,117 @@ while running:
             enemyY[i] += enemyY_co[i]
 
         # collision
+        Death = isCollision(enemyX[i], enemyY[i], playerX, playerY)
+        if Death:
+            pygame.gfxdraw.box(screen, pygame.Rect(0,0,2000,2000), (0,0,0,200))
+            LoseR = LoseT.get_rect()
+            LoseR.center = ((screen.get_width()/2), 300)
+            screen.blit(LoseT, LoseR)
+            Score = str(Score)
+            ScoreT = fontS.render('Your Score was '+Score, True, (240,0,0))
+            ScoreR = ScoreT.get_rect()
+            ScoreR.center = ((screen.get_width()/2), 400)
+            screen.blit(ScoreT, ScoreR)
+            AgainT = fontS.render('Press Space to play again', True, (0,100,0))
+            AgainR = AgainT.get_rect()
+            AgainR.center = ((screen.get_width()/2), 500)
+            screen.blit(AgainT, AgainR)
+            pause = True
+            pygame.display.update()
+            while pause:
+                for task in pygame.event.get():
+                    if task.type == pygame.KEYDOWN:
+                        if task.key == pygame.K_SPACE:
+                            pause = False
+                            #Again!
+                            background_img = pygame.image.load('Background.png')
+                            # Player
+                            player_icon = pygame.image.load('Jet.png')
+                            playerX = (screen.get_width()/2)
+                            playerY = 630
+                            playerX_co = 0
+                            playerY_co = 0
+                            # enemy
+                            enemy_icon = []
+                            spawn = 20
+                            enemyX = []
+                            enemyY = []
+                            enemyX_co = []
+                            enemyY_co = []
+                            Score = 100000
+                            for i in range(spawn):
+                                enemy_icon.append(pygame.image.load('Alien Ship.png'))
+                                enemyX.append(random.randint(((screen.get_width()/4)), ((screen.get_width()/4)*3)))
+                                enemyY.append(random.randint(50, 150))
+                                enemyX_co.append(1)
+                                enemyY_co.append(40)
+                            # bullet
+                            bullet_icon = pygame.image.load('bullet(1).png')
+                            bulletX = 0
+                            bulletY = 630
+                            bulletX_co = 0
+                            bulletY_co = 6
+                            bullet_state = "reload"
         collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
         if collision:
             pygame.mixer.Sound.play(Boom)
             bulletY = 630
             bullet_state = 'reload'
-            enemyX[i] = random.randint(0, 735)
+            enemyX[i] = random.randint(((screen.get_width()/4)), ((screen.get_width()/4)*3))
             enemyY[i] = random.randint(50, 150)
             spawn -= 1
             if spawn == 0:
-                running = False
+                pygame.gfxdraw.box(screen, pygame.Rect(0,0,2000,2000), (0,0,0,200))
+                WinR = WinT.get_rect()
+                WinR.center = ((screen.get_width()/2), 300)
+                screen.blit(WinT, WinR)
+                Score = str(Score)
+                ScoreT = fontS.render('Your Score was '+Score, True, (240,0,0))
+                ScoreR = ScoreT.get_rect()
+                ScoreR.center = ((screen.get_width()/2), 400)
+                screen.blit(ScoreT, ScoreR)
+                AgainT = fontS.render('Press Space to play again', True, (0,100,0))
+                AgainR = AgainT.get_rect()
+                AgainR.center = ((screen.get_width()/2), 500)
+                screen.blit(AgainT, AgainR)
+                pause = True
+                pygame.display.update()
+                while pause:
+                    for task in pygame.event.get():
+                        if task.type == pygame.KEYDOWN:
+                            if task.key == pygame.K_SPACE:
+                                pause = False
+                                #Again!
+                                background_img = pygame.image.load('Background.png')
+                                # Player
+                                player_icon = pygame.image.load('Jet.png')
+                                playerX = (screen.get_width()/2)
+                                playerY = 630
+                                playerX_co = 0
+                                playerY_co = 0
+                                # enemy
+                                enemy_icon = []
+                                enemyX = []
+                                enemyY = []
+                                enemyX_co = []
+                                enemyY_co = []
+                                spawn = 20
+                                Score = 100000
+                                for i in range(spawn):
+                                    enemy_icon.append(pygame.image.load('Alien Ship.png'))
+                                    enemyX.append(random.randint(((screen.get_width()/4)), ((screen.get_width()/4)*3)))
+                                    enemyY.append(random.randint(50, 150))
+                                    enemyX_co.append(1)
+                                    enemyY_co.append(40)
+                                # bullet
+                                bullet_icon = pygame.image.load('bullet(1).png')
+                                bulletX = 0
+                                bulletY = 630
+                                bulletX_co = 0
+                                bulletY_co = 6
+                                bullet_state = 'reload'
+
+
         enemy(enemyX[i], enemyY[i], i)
 
     # bullet movements
